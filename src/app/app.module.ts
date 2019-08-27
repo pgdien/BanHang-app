466ab618799af25263e26bf6c72c4f3d62b8bb44
pgdien <pgdien2@gmail.com>
2019-08-27 23:55:44 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 
 @NgModule({
   declarations: [
@@ -14,11 +10,7 @@ import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    NgZorroAntdMobileModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
